976f8036d824841a8cd46b7e0b2faea958a2f0e1
Utkarsh Karki <utkarsh1998@icloud.com>
2024-09-23 17:54:22 -0400

    Created the Address  model class

--- PoliticalPreparedness-Backend/src/main/java/com/example/PoliticalPreparedness/models/Address.java
+++ PoliticalPreparedness-Backend/src/main/java/com/example/PoliticalPreparedness/models/Address.java
@@ -1,4 +1,33 @@
 package com.example.PoliticalPreparedness.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "address", schema = "pp_web")
+@Data
+@NoArgsConstructor
 public class Address {
+
+    @Id
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "street", length = 100, nullable = false)
+    private String street;
+
+    @Column(name = "city", length = 50, nullable = false)
+    private String city;
+
+    @Column(name = "state", length = 50, nullable = false)
+    private String state;
+
+    @Column(name = "postal_code", length = 20, nullable = false)
+    private String postalCode;
 }
