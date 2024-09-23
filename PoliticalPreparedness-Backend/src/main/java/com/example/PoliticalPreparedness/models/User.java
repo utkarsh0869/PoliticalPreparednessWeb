0b9878bd716e049857417cd7888d0ab3325d9859
Utkarsh Karki <utkarsh1998@icloud.com>
2024-09-23 17:11:51 -0400

    Created the User model class

---
+++ PoliticalPreparedness-Backend/src/main/java/com/example/PoliticalPreparedness/models/User.java
@@ -0,0 +1,26 @@
+package com.example.PoliticalPreparedness.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users", schema = "pp_web")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Let JPA know that the database is responsible for generating the primary key values.
+    private int userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Address address;
+}
