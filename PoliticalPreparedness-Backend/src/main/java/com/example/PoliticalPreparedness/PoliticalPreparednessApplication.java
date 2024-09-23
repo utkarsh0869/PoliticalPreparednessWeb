2a0919b63233fcd44cc73532eef0ba09095ac9cd
Utkarsh Karki <utkarsh1998@icloud.com>
2024-09-23 16:38:43 -0400

    Initial backend files

---
+++ PoliticalPreparedness-Backend/src/main/java/com/example/PoliticalPreparedness/PoliticalPreparednessApplication.java
@@ -0,0 +1,13 @@
+package com.example.PoliticalPreparedness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalPreparednessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticalPreparednessApplication.class, args);
+	}
+
+}
