50a89ff7e2f9ca0c3d88c1e958f37fb7e1c07697
Utkarsh Karki <utkarsh1998@icloud.com>
2024-09-26 16:42:56 -0400

    Front end initial set up.

--- PoliticalPreparedness-Backend/src/main/java/com/example/PoliticalPreparedness/services/CivicsInfoService.java
+++ PoliticalPreparedness-Backend/src/main/java/com/example/PoliticalPreparedness/services/CivicsInfoService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
